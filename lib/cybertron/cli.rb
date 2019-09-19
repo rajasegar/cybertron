800f1f8a440c6ff6003c6b971e2a574de87b2146
Rajasegar <rajasegarc@freshdesk.com>
2019-09-19 15:19:26 +0530

    [CHORE] Adding new and generate commands

--- lib/cybertron/cli.rb
+++ lib/cybertron/cli.rb
@@ -3,26 +3,40 @@
 require 'optparse'
 require 'cybertron/options'
 require 'cybertron/version'
+require 'thor'
+require 'fileutils'
 
 module Cybertron
-  class CLI
-    def initialize
-      @options = Cybertron::Options.new
-      OptionParser.new do |opts|
-        opts.banner = 'Usage: cybertron new <transform-name>'
+  # CLI class
+  class CLI < Thor
+    include Thor::Actions
 
-        opts.on('--version', 'Print version number') do
-          puts Cybertron::VERSION
-          exit
-        end
+    def self.source_root
+      File.expand_path('../templates', __dir__)
+    end
 
-        opts.on('-h', '--help', 'Prints help') do
-          puts opts
-          exit
-        end
+    desc ' new <project-name>', 'Create a new Codemod project'
+    def new(name)
+      puts "Creating new project: #{name}"
+      FileUtils.mkdir_p "#{name}/transforms"
+      FileUtils.mkdir_p "#{name}/spec"
+      template 'rspec.tt', "#{name}/.rspec"
+      template 'spec_helper.tt', "#{name}/spec/spec_helper.rb"
+      template 'Rakefile.tt', "#{name}/Rakefile"
+      template 'readme.tt', "#{name}/README.md"
+    end
 
-
-      end.parse!
+    desc ' generate <name-of-transform>', 'Generate a new transform'
+    def generate(name)
+      @name = name
+      puts "Creating new transform: #{name}"
+      FileUtils.mkdir_p "transforms/#{name}"
+      FileUtils.mkdir_p "transforms/#{name}/fixtures"
+      template 'transform.tt', "transforms/#{name}/transform.rb"
+      template 'transform_spec.tt', "spec/#{name}_spec.rb"
+      template 'fixture_input.tt', "transforms/#{name}/fixtures/basic_input.rb"
+      template 'fixture_output.tt', "transforms/#{name}/fixtures/basic_output.rb"
+      template 'readme_transform.tt', "transforms/#{name}/README.md"
     end
   end
 end
---
+++ lib/cybertron/options.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Cybertron
+  class Options
+
+    def initialize
+    end
+  end
+end
